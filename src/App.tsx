6d8bd4a95ea4bcb43ed515f826c90908065a28df
thecrether@gmail.com <denisimeri6@gmail.com>
2021-04-13 12:33:55 +0200

    :construction: started with building main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-import { RootDispatch, RootState } from "@store";
-import { AppActionTypes } from "@store/app";
-import { useDispatch, useSelector } from "react-redux";
 import css from "./App.module.scss";
+import { Header } from "@components";
 
 export function App(): JSX.Element {
-  const state = useSelector((state: RootState) => ({
-    loading: state.appReducer.loading,
-  }));
-  const dispatch = useDispatch<RootDispatch>();
-  const handleChange = () =>
-    dispatch({
-      type: AppActionTypes.SET_LOADING,
-      payload: !state.loading,
-    });
+  // const state = useSelector((state: RootState) => ({
+  //   loading: state.appReducer.loading,
+  // }));
+  // const dispatch = useDispatch<RootDispatch>();
+  // const handleChange = () =>
+  //   dispatch({
+  //     type: AppActionTypes.SET_LOADING,
+  //     payload: !state.loading,
+  //   });
 
   return (
     <div className={css.App}>
-      <button onClick={handleChange}>
-        {state.loading ? "asjdjksajhks" : "hdjdj"}
-      </button>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import css from "./Header.module.scss";
+
+export const Header = (): JSX.Element => {
+  return <div className={css.header}>lll</div>;
+};
